6fab4975d33435674deb0c5d2205b9d7ef961308
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2021-05-23 16:24:43 -0500

    Added Entry Structure renderer for IFrames and code blocks. Uses PrismJS for code highlighting.

---
+++ Shared/Components/RichTextPageContent/Renders/ContentfulBlocksEmbeddedEntryRender.cs
@@ -0,0 +1,78 @@
+﻿namespace Blazor.Contentful_.Blog.Starter.Shared.Components.RichTextPageContent.Renders
+{
+    using System.Net;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Contentful.Core.Models;
+
+    public class ContentfulBlocksEmbeddedEntryRender
+        : IContentRenderer
+    {
+        public int Order
+        {
+            get;
+            set;
+        } = 100;
+
+        public bool SupportsContent(IContent content)
+        {
+            return content is EntryStructure;
+        }
+        public string Render(IContent content)
+        {
+            var entryStructure = content as EntryStructure;
+            var stringBuilder = new StringBuilder();
+            if (entryStructure?.Data.Target is not CustomNode target)
+            {
+                return string.Empty;
+            }
+
+            var targetTyped = target.JObject.ToObject<CustomNodeTyped>();
+            var contentType = targetTyped.Sys.ContentType.SystemProperties.Id;
+
+            switch (contentType)
+            {
+                case "videoEmbed":
+                    var title = targetTyped.Title;
+                    var embedUrl = targetTyped.EmbedUrl;
+                    stringBuilder.Append("<div class=\"video-embed\"><iframe class=\"video-embed__iframe\" ")
+                        .Append(" src=\"").Append(embedUrl).Append('"')
+                        .Append(" height=\"100%\"")
+                        .Append(" width=\"100%\"")
+                        .Append(" frameBoarder=\"0\"")
+                        .Append(" scrolling=\"no\"")
+                        .Append(" allowFullScreen=\"true\"")
+                        .Append(" title=\"").Append(title).Append('"')
+                        .Append("></iframe></div>");
+                    break;
+                case "codeBlock":
+                    var language = targetTyped.Language;
+                    var code = targetTyped.Code;
+                    stringBuilder.Append("<pre class=\"code-block\"><code class=\"code-block__inner ")
+                        .Append("language-").Append(language)
+                        .Append("\">")
+                        .Append(WebUtility.HtmlEncode(code))
+                        .Append("</code></pre>");
+                    break;
+                default:
+                    break;
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public Task<string> RenderAsync(IContent content)
+        {
+            return Task.FromResult(Render(content));
+        }
+
+        public class CustomNodeTyped
+        {
+            public SystemProperties Sys { get; set; } = new SystemProperties();
+            public string Title { get; set; } = string.Empty;
+            public string EmbedUrl { get; set; } = string.Empty;
+            public string Language { get; set; } = string.Empty;
+            public string Code { get; set; } = string.Empty;
+        }
+    }
+}
--- Shared/Components/RichTextPageContent/RichTextPageContent.razor.cs
+++ Shared/Components/RichTextPageContent/RichTextPageContent.razor.cs
@@ -2,8 +2,10 @@
 {
     using System.Threading.Tasks;
     using Blazor.Contentful_.Blog.Starter.ContentfulSdk.Model.Page;
-    using global::Contentful.Core.Models;
+    using Blazor.Contentful_.Blog.Starter.Shared.Components.RichTextPageContent.Renders;
+    using Contentful.Core.Models;
     using Microsoft.AspNetCore.Components;
+    using Microsoft.JSInterop;
 
     public partial class RichTextPageContent
         : ComponentBase
@@ -11,13 +13,31 @@
         [Parameter]
         public PageBody? Body { get; set; }
 
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; } = null!;
+
+
         public string Content { get; set; } = string.Empty;
 
         protected override async Task OnInitializedAsync()
         {
             if (Body is not null)
             {
-                Content = await new HtmlRenderer().ToHtml(Body);
+                var htmlRenderer = new HtmlRenderer();
+                htmlRenderer.AddRenderer(
+                    new ContentfulBlocksEmbeddedEntryRender()
+                );
+                Content = await htmlRenderer.ToHtml(Body);
+            }
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JSRuntime.InvokeVoidAsync(
+                    "Prisim_highlightAll"
+                );
             }
         }
 
@@ -25,7 +45,11 @@
         {
             if (Body is not null)
             {
-                Content = await new HtmlRenderer().ToHtml(Body);
+                var htmlRenderer = new HtmlRenderer();
+                htmlRenderer.AddRenderer(
+                    new ContentfulBlocksEmbeddedEntryRender()
+                );
+                Content = await htmlRenderer.ToHtml(Body);
             }
         }
     }
